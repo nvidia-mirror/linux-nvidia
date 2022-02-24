a3ab344bfdde30f2d63c9a74dd8379d8f8adce3e
Ken Chang <kenc@nvidia.com>
2022-02-24 17:18:07 +0800

    tegra: camera: vi5: configure atomp for NV16
    'NV16' has two planes (Y and UV) in a single buffer, hence we need to
    configure the additional surface accordingly.
    Also need to set capture channel flags properly for requesting an atomp
    channel with semi-planar capability. This needs to be done before issuing
    IVC CAPTURE_CHANNEL_SETUP_REQ.
    
    Bug 3533581
    
    Change-Id: I7678555adc5cba99ff2f3acdbec0c669cba1b8c3
    Signed-off-by: Ken Chang <kenc@nvidia.com>
    (cherry picked from commit 3463f99b2a3d397da7e2c01adfb2c8a8c97a87be)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2678534
    (cherry picked from commit f01fa07c5eb799f40d32d52f3f7fc2b656358e53)
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2725739
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: svc-mobile-coverity <svc-mobile-coverity@nvidia.com>
    Reviewed-by: svc-mobile-cert <svc-mobile-cert@nvidia.com>
    Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>
    Reviewed-by: Anubhav Rai <arai@nvidia.com>
    Reviewed-by: Bibek Basu <bbasu@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/media/platform/tegra/camera/vi/vi5_fops.c
+++ drivers/media/platform/tegra/camera/vi/vi5_fops.c
@@ -304,6 +304,9 @@ static int tegra_channel_capture_setup(struct tegra_channel *chan, unsigned int 
 		setup.csi_port = csi_chan->ports[vi_port].csi_port;
 	}
 
+	if (chan->fmtinfo->fourcc == V4L2_PIX_FMT_NV16)
+		setup.channel_flags |= CAPTURE_CHANNEL_FLAG_SEMI_PLANAR;
+
 	err = vi_capture_setup(chan->tegra_vi_channel[vi_port], &setup);
 	if (err) {
 		dev_err(chan->vi->dev, "vi capture setup failed\n");
@@ -353,6 +356,11 @@ static void vi5_setup_surface(struct tegra_channel *chan,
 	desc_memoryinfo->surface[0].base_address = offset;
 	desc_memoryinfo->surface[0].size = chan->format.bytesperline * height;
 	desc->ch_cfg.atomp.surface_stride[0] = bpl;
+	if (chan->fmtinfo->fourcc == V4L2_PIX_FMT_NV16) {
+		desc_memoryinfo->surface[1].base_address = offset + chan->format.sizeimage / 2;
+		desc_memoryinfo->surface[1].size = chan->format.bytesperline * height;
+		desc->ch_cfg.atomp.surface_stride[1] = bpl;
+	}
 
 	if (chan->embedded_data_height > 0) {
 		desc->ch_cfg.embdata_enable = 1;
