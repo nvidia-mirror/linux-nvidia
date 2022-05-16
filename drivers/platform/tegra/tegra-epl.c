bd507a6e8344ccfc59cec9b7d4eee6cfe3238861
Rahul Bedarkar <rabedarkar@nvidia.com>
2022-05-16 06:38:01 +0000

    tegra-epl: Sync-up changes from OOT driver
    Sync-up changes from OOT driver into mainline so that any changes from
    mainline can be synced up easily later.
    
    JIRA SS-3767
    
    Change-Id: Ica2427558e5d44e253340d4e3e09fb313fa2da67
    Signed-off-by: Rahul Bedarkar <rabedarkar@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2714991
    Reviewed-by: Sidharth Varier <svarier@nvidia.com>
    Reviewed-by: Shubham Jain <shubhamj@nvidia.com>
    Reviewed-by: Adithya Manjunath <amanjunath@nvidia.com>
    Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- drivers/platform/tegra/tegra-epl.c
+++ drivers/platform/tegra/tegra-epl.c
@@ -1,28 +1,6 @@
-/*
- * Copyright (c) 2021-2022, NVIDIA CORPORATION. All rights reserved.
- *
- * This software is licensed under the terms of the GNU General Public
- * License version 2, as published by the Free Software Foundation, and
- * may be copied, distributed, and modified under those terms.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- */
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (c) 2021-2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
-/**
- * @file tegra-epl.c
- * @brief <b> epl driver to communicate with eplcom daemon</b>
- *
- * This file will register as client driver so that EPL client can
- * report SW error to FSI using HSP mailbox from user space
- */
-
-/* ==================[Includes]============================================= */
-
-#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/types.h>
@@ -32,56 +10,28 @@
 #include <linux/device.h>
 #include <linux/kdev_t.h>
 #include <linux/mailbox_client.h>
-#include <linux/sched/signal.h>
-#include "linux/tegra-epl.h"
-#include "uapi/linux/tegra-epl.h"
+#include <linux/tegra-epl.h>
 #include <linux/pm.h>
 
-/*Timeout in millisec*/
+/* Timeout in milliseconds */
 #define TIMEOUT		1000
 
-/*32bit data Length*/
+/* 32bit data Length */
 #define MAX_LEN	4
 
-/* Macro indicating misc register width */
-#define MISC_REG_WIDTH    4U
-
-/* Macro indiciating total number of Misc Sw generic errors in Misc EC */
+/* Macro indicating total number of Misc Sw generic errors in Misc EC */
 #define NUM_SW_GENERIC_ERR 5U
 
-/* Macro for Misc register access, because of guardword check could not
- * include the hw headers here.
- */
+/* Error index offset in mission status register */
+#define ERROR_INDEX_OFFSET	24U
 
-/* Macro for Misc EC mission error status register address */
-#define MISC_EC_ERRSLICE0_MISSIONERR_STATUS_0   0x024e0038U
-
-/* Macro for Misc registers */
-
-#define MISCREG_MISC_EC_ERR0_SW_ERR_CODE_0      0x00110000U
-#define MISCREG_MISC_EC_ERR0_SW_ERR_ASSERT_0    0x00110004U
-
-#define MISCREG_MISC_EC_ERR1_SW_ERR_CODE_0      0x00120000U
-#define MISCREG_MISC_EC_ERR1_SW_ERR_ASSERT_0    0x00120004U
-
-#define MISCREG_MISC_EC_ERR2_SW_ERR_CODE_0      0x00130000U
-#define MISCREG_MISC_EC_ERR2_SW_ERR_ASSERT_0    0x00130004U
-
-#define MISCREG_MISC_EC_ERR3_SW_ERR_CODE_0      0x00140000U
-#define MISCREG_MISC_EC_ERR3_SW_ERR_ASSERT_0    0x00140004U
-
-#define MISCREG_MISC_EC_ERR4_SW_ERR_CODE_0      0x00150000U
-#define MISCREG_MISC_EC_ERR4_SW_ERR_ASSERT_0    0x00150004U
-
-/* =================[Data types]======================================== */
-
-/*Data type for mailbox client and channel details*/
+/* Data type for mailbox client and channel details */
 struct epl_hsp_sm {
 	struct mbox_client client;
 	struct mbox_chan *chan;
 };
 
-/*Data type for accessing TOP2 HSP */
+/* Data type for accessing TOP2 HSP */
 struct epl_hsp {
 	struct epl_hsp_sm tx;
 	struct device dev;
@@ -89,17 +39,10 @@ struct epl_hsp {
 
 /* Data type to store Misc Sw Generic error configuration */
 struct epl_misc_sw_err_cfg {
-	uint32_t err_code_phyaddr;
-	uint32_t err_assert_phy_addr;
 	void __iomem *err_code_va;
 	void __iomem *err_assert_va;
 	const char *dev_configured;
-	uint8_t ec_err_idx;
 };
-
-/* =================[GLOBAL variables]================================== */
-static ssize_t device_file_ioctl(
-		struct file *, unsigned int cmd, unsigned long arg);
 
 static int device_file_major_number;
 static const char device_name[] = "epdaemon";
@@ -112,71 +55,7 @@ static void __iomem *mission_err_status_va;
 
 static bool isAddrMappOk = true;
 
-static struct epl_misc_sw_err_cfg miscerr_cfg[NUM_SW_GENERIC_ERR] = {
-	{
-		.err_code_phyaddr = MISCREG_MISC_EC_ERR0_SW_ERR_CODE_0,
-		.err_assert_phy_addr = MISCREG_MISC_EC_ERR0_SW_ERR_ASSERT_0,
-		.ec_err_idx = 24U,
-	},
-	{
-		.err_code_phyaddr = MISCREG_MISC_EC_ERR1_SW_ERR_CODE_0,
-		.err_assert_phy_addr = MISCREG_MISC_EC_ERR1_SW_ERR_ASSERT_0,
-		.ec_err_idx = 25U,
-	},
-	{
-		.err_code_phyaddr = MISCREG_MISC_EC_ERR2_SW_ERR_CODE_0,
-		.err_assert_phy_addr = MISCREG_MISC_EC_ERR2_SW_ERR_ASSERT_0,
-		.ec_err_idx = 26U,
-	},
-	{
-		.err_code_phyaddr = MISCREG_MISC_EC_ERR3_SW_ERR_CODE_0,
-		.err_assert_phy_addr = MISCREG_MISC_EC_ERR3_SW_ERR_ASSERT_0,
-		.ec_err_idx = 27U,
-	},
-	{
-		.err_code_phyaddr = MISCREG_MISC_EC_ERR4_SW_ERR_CODE_0,
-		.err_assert_phy_addr = MISCREG_MISC_EC_ERR4_SW_ERR_ASSERT_0,
-		.ec_err_idx = 28U,
-	}
-};
-
-/*File operations*/
-const static struct file_operations epl_driver_fops = {
-	.owner   = THIS_MODULE,
-	.unlocked_ioctl   = device_file_ioctl,
-};
-
-static int epl_register_device(void)
-{
-	int result = 0;
-	struct class *dev_class;
-
-	result = register_chrdev(0, device_name, &epl_driver_fops);
-	if (result < 0) {
-		pr_err("%s> register_chrdev code = %i\n", device_name, result);
-		return result;
-	}
-	device_file_major_number = result;
-	dev_class = class_create(THIS_MODULE, device_name);
-	if (dev_class == NULL) {
-		pr_err("%s> Could not create class for device\n", device_name);
-		goto class_fail;
-	}
-
-	if ((device_create(dev_class, NULL,
-		MKDEV(device_file_major_number, 0),
-			 NULL, device_name)) == NULL) {
-		pr_err("%s> Could not create device node\n", device_name);
-		goto device_failure;
-	}
-	return 0;
-
-device_failure:
-	class_destroy(dev_class);
-class_fail:
-	unregister_chrdev(device_file_major_number, device_name);
-	return -1;
-}
+static struct epl_misc_sw_err_cfg miscerr_cfg[NUM_SW_GENERIC_ERR];
 
 static void tegra_hsp_tx_empty_notify(struct mbox_client *cl,
 					 void *data, int empty_value)
@@ -202,17 +81,10 @@ static int tegra_hsp_mb_init(struct device *dev)
 	if (IS_ERR(epl_hsp_v->tx.chan)) {
 		err = PTR_ERR(epl_hsp_v->tx.chan);
 		dev_err(dev, "failed to get tx mailbox: %d\n", err);
-		devm_kfree(dev, epl_hsp_v);
 		return err;
 	}
 
 	return 0;
-}
-
-static void epl_unregister_device(void)
-{
-	if (device_file_major_number != 0)
-		unregister_chrdev(device_file_major_number, device_name);
 }
 
 static ssize_t device_file_ioctl(
@@ -256,7 +128,7 @@ int epl_get_misc_ec_err_status(struct device *dev, uint8_t err_number, bool *sta
 		if (strcmp(dev_str, miscerr_cfg[err_number].dev_configured) != 0)
 			return -EACCES;
 
-		mask = (1U << (miscerr_cfg[err_number].ec_err_idx % 32U));
+		mask = (1U << ((ERROR_INDEX_OFFSET + err_number) % 32U));
 		mission_err_status = readl(mission_err_status_va);
 
 		if ((mission_err_status & mask) != 0U)
@@ -269,7 +141,7 @@ int epl_get_misc_ec_err_status(struct device *dev, uint8_t err_number, bool *sta
 
 	return ret;
 }
-EXPORT_SYMBOL(epl_get_misc_ec_err_status);
+EXPORT_SYMBOL_GPL(epl_get_misc_ec_err_status);
 
 int epl_report_misc_ec_error(struct device *dev, uint8_t err_number,
 	uint32_t sw_error_code)
@@ -293,7 +165,7 @@ int epl_report_misc_ec_error(struct device *dev, uint8_t err_number,
 
 	return 0;
 }
-EXPORT_SYMBOL(epl_report_misc_ec_error);
+EXPORT_SYMBOL_GPL(epl_report_misc_ec_error);
 
 int epl_report_error(struct epl_error_report_frame error_report)
 {
@@ -306,7 +178,7 @@ int epl_report_error(struct epl_error_report_frame error_report)
 
 	return ret < 0 ? ret : 0;
 }
-EXPORT_SYMBOL(epl_report_error);
+EXPORT_SYMBOL_GPL(epl_report_error);
 
 static int __maybe_unused epl_client_suspend(struct device *dev)
 {
@@ -327,6 +199,50 @@ static const struct of_device_id epl_client_dt_match[] = {
 };
 
 MODULE_DEVICE_TABLE(of, epl_client_dt_match);
+
+/* File operations */
+static const struct file_operations epl_driver_fops = {
+	.owner   = THIS_MODULE,
+	.unlocked_ioctl   = device_file_ioctl,
+};
+
+static int epl_register_device(void)
+{
+	int result = 0;
+	struct class *dev_class;
+
+	result = register_chrdev(0, device_name, &epl_driver_fops);
+	if (result < 0) {
+		pr_err("%s> register_chrdev code = %i\n", device_name, result);
+		return result;
+	}
+	device_file_major_number = result;
+	dev_class = class_create(THIS_MODULE, device_name);
+	if (dev_class == NULL) {
+		pr_err("%s> Could not create class for device\n", device_name);
+		goto class_fail;
+	}
+
+	if ((device_create(dev_class, NULL,
+		MKDEV(device_file_major_number, 0),
+			 NULL, device_name)) == NULL) {
+		pr_err("%s> Could not create device node\n", device_name);
+		goto device_failure;
+	}
+	return 0;
+
+device_failure:
+	class_destroy(dev_class);
+class_fail:
+	unregister_chrdev(device_file_major_number, device_name);
+	return -1;
+}
+
+static void epl_unregister_device(void)
+{
+	if (device_file_major_number != 0)
+		unregister_chrdev(device_file_major_number, device_name);
+}
 
 static int epl_client_probe(struct platform_device *pdev)
 {
@@ -349,15 +265,15 @@ static int epl_client_probe(struct platform_device *pdev)
 
 			/* Mapping registers to process address space */
 			miscerr_cfg[iterator].err_code_va =
-				ioremap(miscerr_cfg[iterator].err_code_phyaddr, MISC_REG_WIDTH);
+				devm_platform_ioremap_resource(pdev, (iterator * 2));
 			miscerr_cfg[iterator].err_assert_va =
-				ioremap(miscerr_cfg[iterator].err_assert_phy_addr, MISC_REG_WIDTH);
+				devm_platform_ioremap_resource(pdev, (iterator * 2) + 1);
 
-			if ((miscerr_cfg[iterator].err_code_va == NULL) ||
-					(miscerr_cfg[iterator].err_assert_va == NULL)) {
+			if (IS_ERR(miscerr_cfg[iterator].err_code_va) ||
+					IS_ERR(miscerr_cfg[iterator].err_assert_va)) {
 				isAddrMappOk = false;
 				ret = -1;
-				pr_info("epl: error in mapping misc err register for err #%d\n",
+				dev_err(&pdev->dev, "error in mapping misc err register for err #%d\n",
 						iterator);
 			}
 		} else {
@@ -365,11 +281,11 @@ static int epl_client_probe(struct platform_device *pdev)
 		}
 	}
 
-	mission_err_status_va = ioremap(MISC_EC_ERRSLICE0_MISSIONERR_STATUS_0, MISC_REG_WIDTH);
-	if (mission_err_status_va == NULL) {
-		ret = -1;
+	mission_err_status_va = devm_platform_ioremap_resource(pdev, NUM_SW_GENERIC_ERR * 2);
+	if (IS_ERR(mission_err_status_va)) {
 		isAddrMappOk = false;
-		pr_info("epl: error in mapping mission error status register\n");
+		dev_err(&pdev->dev, "error in mapping mission error status register\n");
+		return PTR_ERR(mission_err_status_va);
 	}
 
 	return ret;
@@ -377,15 +293,7 @@ static int epl_client_probe(struct platform_device *pdev)
 
 static int epl_client_remove(struct platform_device *pdev)
 {
-	int iterator = 0;
-
 	epl_unregister_device();
-	devm_kfree(&pdev->dev, epl_hsp_v);
-	iounmap(mission_err_status_va);
-	for (iterator = 0; iterator < NUM_SW_GENERIC_ERR; iterator++) {
-		iounmap(miscerr_cfg[iterator].err_code_va);
-		iounmap(miscerr_cfg[iterator].err_assert_va);
-	}
 	return 0;
 }
 
