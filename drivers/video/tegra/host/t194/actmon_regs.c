bc6f5ebf5d1d13385acf63743d3d1ea838c7a721
Johnny Liu <johnliu@nvidia.com>
2022-06-16 15:52:58 +0800

    drivers: actmon: default register values for nvdec
    Specify the default values for actmon registers explicitly.
    
    Bug 200493763
    
    Change-Id: I376fd66558b59aefb4477d990459d64461ce81e6
    Signed-off-by: Johnny Liu <johnliu@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2729782
    Reviewed-by: svc_kernel_abi <svc_kernel_abi@nvidia.com>
    Reviewed-by: David Ung <davidu@nvidia.com>
    Reviewed-by: Bitan Biswas <bbiswas@nvidia.com>
    GVS: Gerrit_Virtual_Submit <buildbot_gerritrpt@nvidia.com>
    Tested-by: Andrew Chen <andrewc@nvidia.com>

--- drivers/video/tegra/host/t194/actmon_regs.c
+++ drivers/video/tegra/host/t194/actmon_regs.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2021, NVIDIA Corporation.  All rights reserved.
+ * Copyright (c) 2019-2022, NVIDIA Corporation.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,8 +23,39 @@ static struct nvhost_actmon_register __attribute__((__unused__))
 
 static struct nvhost_actmon_register __attribute__((__unused__))
 	t19x_nvdec_actmon_registers[] = {
-	/* NV_PNVDEC_TFBIF_ACTMON_ACTIVE_WEIGHT */
-	{ .addr = 0x00002c54, .val = 0x000000f8 },
+	/*
+	 * NV_PNVDEC_TFBIF_ACTMON_ACTIVE_MASK
+	 * - Mask out signals with associated bit field value equals to 1
+	 * - bit fields = active,delayed,stalled,starved
+	 * - bit representation = 0111
+	 * - STARVED signal is not used in nvdec
+	 */
+	{
+		.addr = 0x00002c4c,
+		.val = 0x00000007
+	},
+	/*
+	 * NV_PNVDEC_TFBIF_ACTMON_ACTIVE_BORPS
+	 * - 2 bits are used to describe the usage of each signal
+	 * - bit #0 => POLARITY, 1=NEGATIVE, 0=POSITIVE
+	 * - bit #1 => OPERATION, 1=AND, 0=OR
+	 * - bit fields = active,delayed,stalled,starved
+	 * - bit representation = 10000000
+	 */
+	{
+		.addr = 0x00002c50,
+		.val = 0x00000080
+	},
+	/*
+	 * NV_PNVDEC_TFBIF_ACTMON_ACTIVE_WEIGHT
+	 * - 32 bits are used to specify the count weight
+	 * - NVDEC will toggle active signal every `count_weight` cycle time
+	 * - count_weight formula = nvdec_fmax / actmon_clk * 4
+	 */
+	{
+		.addr = 0x00002c54,
+		.val = 0x000000f8
+	},
 	{}
 };
 
--- drivers/video/tegra/host/t23x/actmon_regs.c
+++ drivers/video/tegra/host/t23x/actmon_regs.c
@@ -23,8 +23,39 @@ static struct nvhost_actmon_register __attribute__((__unused__))
 
 static struct nvhost_actmon_register __attribute__((__unused__))
 	t23x_nvdec_actmon_registers[] = {
-	/* NV_PNVDEC_TFBIF_ACTMON_ACTIVE_WEIGHT */
-	{ .addr = 0x00004e54, .val = 0x000000d8 },
+	/*
+	 * NV_PNVDEC_TFBIF_ACTMON_ACTIVE_MASK
+	 * - Mask out signals with associated bit field value equals to 1
+	 * - bit fields = active,delayed,stalled,starved
+	 * - bit representation = 0111
+	 * - STARVED signal is not used in nvdec
+	 */
+	{
+		.addr = 0x00004e4c,
+		.val = 0x00000007
+	},
+	/*
+	 * NV_PNVDEC_TFBIF_ACTMON_ACTIVE_BORPS
+	 * - 2 bits are used to describe the usage of each signal
+	 * - bit #0 => POLARITY, 1=NEGATIVE, 0=POSITIVE
+	 * - bit #1 => OPERATION, 1=AND, 0=OR
+	 * - bit fields = active,delayed,stalled,starved
+	 * - bit representation = 10000000
+	 */
+	{
+		.addr = 0x00004e50,
+		.val = 0x00000080
+	},
+	/*
+	 * NV_PNVDEC_TFBIF_ACTMON_ACTIVE_WEIGHT
+	 * - 32 bits are used to specify the count weight
+	 * - NVDEC will toggle active signal every `count_weight` cycle time
+	 * - count_weight formula = nvdec_fmax / actmon_clk * 4
+	 */
+	{
+		.addr = 0x00004e54,
+		.val = 0x000000d0
+	},
 	{}
 };
 
