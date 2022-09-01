5e98d923372d5d5a7b293ad837f7d5a8888e114d
omar nemri <onemri@nvidia.com>
2022-08-31 19:41:00 -0700

    drivers: pva: t23x support in sidecar
    add support for t23x with sidecar.
    
    - call of_platform_default_populate in order for
      context devices to be probed.
    - adjust h1x LIC enablement flag dynamically for T23x or T19x
    - register context devices
    
    Bug 3755359
    
    Change-Id: I02b751c0200a49ad54f6a7d057c4e2c293865cf2
    Signed-off-by: omar nemri <onemri@nvidia.com>
    Reviewed-on: https://git-master.nvidia.com/r/c/linux-nvidia/+/2769763
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>

--- drivers/video/tegra/host/pva/pva_regs.h
+++ drivers/video/tegra/host/pva/pva_regs.h
@@ -37,11 +37,8 @@
 #define SEC_LIC_INTR_HSP3	0x4
 #define SEC_LIC_INTR_HSP4	0x8
 #define SEC_LIC_INTR_HSP_ALL	0xF
-#ifdef CONFIG_TEGRA_T23X_GRHOST
-#define SEC_LIC_INTR_H1X_ALL	0x3
-#else
-#define SEC_LIC_INTR_H1X_ALL	0x7
-#endif
+#define SEC_LIC_INTR_H1X_ALL_23	0x3
+#define SEC_LIC_INTR_H1X_ALL_19	0x7
 
 /* Watchdog support */
 #define SEC_LIC_INTR_WDT	0x1
